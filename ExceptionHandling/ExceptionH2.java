ef637fdd02f24758d1475371f8f2cdb249c3a4b9
pratap-jaihind <jaihind09920@gmail.com>
2025-06-20 14:58:22 +0530

    This has been handeled by user then you have to use try and catch block

--- ExceptionHandling/ExceptionH2.java
+++ ExceptionHandling/ExceptionH2.java
@@ -1,8 +1,24 @@
 import java.io.IOException;
 
 class ExceptionH2 {
-    public static void main(String[] args) throws IOException { 
-        System.out.println("This is the main method.");
-        throw new IOException("This is an IOException thrown from the main method.");
+    // public static void main(String[] args) throws IOException { 
+    //     System.out.println("This is the main method.");
+    //     throw new IOException("This is an IOException thrown from the main method.");
+    // }
+    public static void main(String[] args){
+       try{
+         throw new IOException("This is an IOException thrown from the main method.");
+       }
+       catch(IOException e){
+           System.out.println("Caught an IOException: " + e.getMessage());
+       }
+       catch(Exception e){
+           System.out.println("Caught a general exception: " + e.getMessage());
+       }
+       finally{
+           System.out.println("This block always executes, regardless of an exception.");
+
+       }
+
     }
 }
