c5c84c77c8ebe00d2021eba2474997e20131ab79
pratap-jaihind <jaihind09920@gmail.com>
2025-06-21 08:03:47 +0530

    Creating a file if not exist

---
+++ FileHandling/FileH1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class FileH1 {
+    public static void main(String[] args) {
+        try {
+            File file = new File("example.txt");
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while creating the file.");
+            e.printStackTrace();
+        }
+    }
+}
