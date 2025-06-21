d601d5fd6e22f117948d7bea6beadb35b8fb16f5
pratap-jaihind <jaihind09920@gmail.com>
2025-06-21 08:38:00 +0530

    A Data has been written in the file

--- FileHandling/FileH1.java
+++ FileHandling/FileH1.java
@@ -2,16 +2,27 @@ import java.io.*;
 
 class FileH1 {
     public static void main(String[] args) {
+        // try {
+        //     File file = new File("example.txt");
+        //     if (file.createNewFile()) {
+        //         System.out.println("File created: " + file.getName());
+        //     } else {
+        //         System.out.println("File already exists.");
+        //     }
+        // } catch (IOException e) {
+        //     System.out.println("An error occurred while creating the file.");
+        //     e.printStackTrace();
+        // }
+        String s1 = "Hello, this is a second test file \n";
+        
         try {
-            File file = new File("example.txt");
-            if (file.createNewFile()) {
-                System.out.println("File created: " + file.getName());
-            } else {
-                System.out.println("File already exists.");
-            }
+            FileOutputStream fos = new FileOutputStream("example.txt" , true);
+            fos.write(s1.getBytes());
+            System.out.println("Data written to file successfully.");
+             fos.close();
         } catch (IOException e) {
-            System.out.println("An error occurred while creating the file.");
+            System.out.println("An error occurred while writing to the file.");
             e.printStackTrace();
-        }
+        } 
     }
 }
