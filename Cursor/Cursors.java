0ff39416eef123e79e868cc45d807471015db510
pratap-jaihind <jaihind09920@gmail.com>
2025-06-23 07:38:35 +0530

    Use of Iterator in Cursor

--- Cursor/Cursors.java
+++ Cursor/Cursors.java
@@ -8,9 +8,14 @@ class Cursors {
         v1.add("Java");
         v1.add("Cursors");
         v1.add("Example");
-        Enumeration<String> e = v1.elements();
-        while (e.hasMoreElements()) {
-            System.out.println(e.nextElement());  // Used for priting the elements of the vector
+        // Enumeration<String> e = v1.elements();
+        // while (e.hasMoreElements()) {
+        //     System.out.println(e.nextElement());  // Used for priting the elements of the vector
+        // }
+
+        Iterator<String> it = v1.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());  // Used for printing the elements of the vector
         }
     }
 }
